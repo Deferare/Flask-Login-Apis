4d264cfe93d4d184cf46e75cf82ad2d00e0ea98e
KANG SEUNG HOON <deforeturn@gmail.com>
2021-08-21 16:11:24 +0900

    백엔드 적인건 대충 완성.
    구글 login api 받아서 json파일의 키 값으로 로그인 request, respones하고
    session 확인 한 다음 로그인 로그아웃 시스템 구현.

--- GoogleLogin/app.py
+++ GoogleLogin/app.py
@@ -1,39 +1,84 @@
-# https://www.youtube.com/watch?v=FKgJEfrhU1E
+import os
+import pathlib
 
-from os import abort
+import requests
+from flask import Flask, session, abort, redirect, request
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
 
-from flask import Flask, session
-
-app = Flask("google login app")
+app = Flask("Google Login App")
 app.secret_key = "CodeSpecialist.com"
 
-def login_is_required(funtion):
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+# 구글 api와의 통신에 필요한 키와 id.
+GOOGLE_CLIENT_ID = "327523895711-fpjhkg2utce0keqbrt568dve63c4q02n.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secrets.json")
+
+# google login api 설정.
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email",
+            "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
+
+# 로그인 상태 확인 함수.
+def login_is_required(function):
     def wrapper(*args, **kwargs):
         if "google_id" not in session:
             return abort(401)
         else:
-            return funtion()
-    return wrapper()
+            return function()
+    return wrapper
 
+# 로그인 버튼 눌렀을때,
 @app.route("/login")
 def login():
-    pass
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
 
 @app.route("/callback")
 def callback():
-    pass
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/protected_area")
 
 @app.route("/logout")
 def logout():
-    pass
+    session.clear() # 세션 초기화.
+    return redirect("/")
 
+# 첫 화면.
 @app.route("/")
 def index():
-    return "Hello Google Login"
+    return "Hello World <a href='/login'><button>Login</button></a>"
 
 @app.route("/protected_area")
+@login_is_required # 여기에 접근을 하기 위해선 login_is_required 함수로 권한 확인.
 def protected_area():
-    pass
+    return f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
 
+# 개발자 모드로 실행.
 if __name__ == "__main__":
     app.run(debug=True)
